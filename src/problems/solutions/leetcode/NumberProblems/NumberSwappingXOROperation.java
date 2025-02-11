512d617460146f314afcd857cf20feea9ed62451
ranjanlahiri <ranjan.lahiri@gmail.com>
2025-02-11 18:46:20 +0530

    number swapping using bit operation

---
+++ src/problems/solutions/leetcode/NumberProblems/NumberSwappingXOROperation.java
@@ -0,0 +1,19 @@
+package problems.solutions.leetcode.NumberProblems;
+
+public class NumberSwappingXOROperation {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 20;
+        
+        System.out.println("Before swapping, a = " + a + " and b = " + b);
+        
+        // Swapping a and b using XOR
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        
+        System.out.println("After swapping, a = " + a + " and b = " + b);
+	}
+
+}
